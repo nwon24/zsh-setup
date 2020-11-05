86af3edb2a203b3eec85ec1d107160c72f958f76
nwon24 <60217282+nwon24@users.noreply.github.com>
2020-11-05 22:08:27 +1100

    Update basic setup (Arch).sh

--- basic setup (Arch).sh
+++ basic setup (Arch).sh
@@ -1,6 +1,5 @@
 cd ~
 yay -S zsh zsh-syntax-highlighting autojump zsh-autosuggestions -yy
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 echo "PS1='%n@%m %~$'" >> ~/.zshrc
-echo "source ~/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+echo "source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
 chsh $USER
